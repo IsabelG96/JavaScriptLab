0aca7c8e240a1409a69264c8f47fedcbaec29cdc
Khalid Hussein <khalhussi@gmail.com>
2023-05-11 16:53:30 +0100

    MVP + 1 extension

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -7,27 +7,48 @@ enter.addEventListener("click", event =>{
     console.log(newItem);
 
     const listItem1 = document.createElement("li");
-    // const listItem2 = document.createElement("li");
-    // const listItem3 = document.createElement("li");
-    
+    const deleteButton = document.createElement("button");
+    const completeButton = document.createElement("button");
     
     listItem1.innerText = newItem;
-    // listItem2.innerText = "Build a wooden house";
-    // listItem3.innerText = "Build a brick house";
+    deleteButton.innerText = "delete";
+    completeButton.innerText = "Mark as complete";
+    
+    listItem1.appendChild(deleteButton);
+    listItem1.appendChild(completeButton);
 
 
     let list = document.getElementById("list");
     list.appendChild(listItem1);
-    // list.appendChild(listItem2);
-    // list.appendChild(listItem3);
+
+    deleteButton.addEventListener("click", event =>{
+        console.log("button clicked");
+        list.removeChild(listItem1);
+    });
+    completeButton.addEventListener("click", event =>{
+        listItem1.setAttribute("style", "color: green;")
+    });
+    });
+
+    
+
+    // create a new element (deleteButton) similar to line 9
+    // create a function that deletes an element (needs to accept an element)
+    // change the inner text to delete
+    // append delete button to listitem1 via list
+    // add event listener to button
+    // remove child (listItem1)
 
 
-    })
+    // create a new element (completebutton)
+    // change inner text to replace
+    // append completebuttom to listitem1
+    // create function for completebutton
+    // add event listener
+    // change color of listitem1
 
-    delete.addEventListener("click", event =>{
-    list.removeChild(listItem1);
 
-})
+    
 
 
 
