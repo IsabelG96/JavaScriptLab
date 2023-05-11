92f2a181f34b73c8afd131675161addfb105ae93
Isabel <isabelgalwey@gmail.com>
2023-05-11 15:15:48 +0100

    completed add item

--- lab_jsevents_start_code/script.js
+++ lab_jsevents_start_code/script.js
@@ -2,24 +2,33 @@ const enter = document.getElementById("enter");
 
 enter.addEventListener("click", event =>{
 
-    const newItem = document.getElementById("new-todo");
-    newItem.querySelector("#new-todo").innerHTML="Hello World!";
+    const newItem = document.getElementById("new-todo").value;
 
-    // const listItem1 = document.createElement("li");
+    console.log(newItem);
+
+    const listItem1 = document.createElement("li");
     // const listItem2 = document.createElement("li");
     // const listItem3 = document.createElement("li");
     
     
-    // listItem1.innerText = newItem;
+    listItem1.innerText = newItem;
     // listItem2.innerText = "Build a wooden house";
     // listItem3.innerText = "Build a brick house";
 
 
-    // let list = document.getElementById("list");
-
-    list.appendChild(newItem);
+    let list = document.getElementById("list");
+    list.appendChild(listItem1);
     // list.appendChild(listItem2);
     // list.appendChild(listItem3);
 
+
+    })
+
+    delete.addEventListener("click", event =>{
+    list.removeChild(listItem1);
+
 })
+
+
+
 
